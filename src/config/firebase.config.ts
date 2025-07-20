a385d38ac69f506ed11f40b435c04c0e28bc4e9a
Ranyll <ranyllpuntanar@gmail.com>
2025-07-20 22:08:26 +0400

    feat: dynamically detect creds on firebase

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,8 +1,13 @@
 import * as admin from 'firebase-admin'
 
-const serviceAccount = require(
-  process.env.FIREBASE_SERVICE_ACCOUNT_PATH as string,
-)
+const serviceAccountPath = `${process.env.FIREBASE_SERVICE_ACCOUNT_PATH}`
+let serviceAccount: admin.ServiceAccount
+
+try {
+  serviceAccount = JSON.parse(serviceAccountPath)
+} catch (e) {
+  serviceAccount = require(serviceAccountPath)
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
