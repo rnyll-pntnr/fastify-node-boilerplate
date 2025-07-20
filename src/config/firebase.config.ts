8180cce7e41787239eed14d57b2a1454b0c83a00
Ranyll <ranyllpuntanar@gmail.com>
2025-07-20 22:18:16 +0400

    feat: revise how firebase auth init

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,16 +1,11 @@
 import * as admin from 'firebase-admin'
 
-const serviceAccountPath = `${process.env.FIREBASE_SERVICE_ACCOUNT_PATH}`
-let serviceAccount: admin.ServiceAccount
-
-try {
-  serviceAccount = JSON.parse(serviceAccountPath)
-} catch (e) {
-  serviceAccount = require(serviceAccountPath)
-}
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  }),
 })
 
 export default admin
